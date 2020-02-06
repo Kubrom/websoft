6d179b9a07c7bb0db9972c7b85cb344e02f4c08a
kubrom <kubrom.mulugheta@gmail.com>
2020-02-06 13:20:37 +0100

    assigment 03

---
+++ work/report/js/flags.js
@@ -0,0 +1,42 @@
+document.getElementById("italyLink").onclick = function() {
+  document.getElementById("color1").style.backgroundColor = "green";
+  document.getElementById("color2").style.backgroundColor = "white";
+  document.getElementById("color3").style.backgroundColor = "red";
+  document.getElementById("image").style.display = "block";
+  document.getElementById("image").style.opacity = "1";
+};
+
+document.getElementById("franceLink").onclick = function() {
+  document.getElementById("color1").style.backgroundColor = "blue";
+  document.getElementById("color2").style.backgroundColor = "white";
+  document.getElementById("color3").style.backgroundColor = "red";
+  document.getElementById("image").style.display = "block";
+  document.getElementById("image").style.opacity = "1";
+};
+
+document.getElementById("guineaLink").onclick = function() {
+  document.getElementById("color1").style.backgroundColor = "red";
+  document.getElementById("color2").style.backgroundColor = "yellow";
+  document.getElementById("color3").style.backgroundColor = "green";
+  document.getElementById("image").style.display = "block";
+  document.getElementById("image").style.opacity = "1";
+};
+document.getElementById("belgiumLink").onclick = function() {
+  document.getElementById("color1").style.backgroundColor = "black";
+  document.getElementById("color2").style.backgroundColor = "yellow";
+  document.getElementById("color3").style.backgroundColor = "red";
+  document.getElementById("image").style.display = "block";
+  document.getElementById("image").style.opacity = "1";
+};
+document.getElementById("nigeriaLink").onclick = function() {
+  document.getElementById("color1").style.backgroundColor = "green";
+  document.getElementById("color2").style.backgroundColor = "white";
+  document.getElementById("color3").style.backgroundColor = "green";
+  document.getElementById("image").style.display = "block";
+  document.getElementById("image").style.opacity = "1";
+};
+
+document.getElementById("image").onclick = function() {
+  document.getElementById("image").style.opacity = "0";
+  document.getElementById("image").style.display = "none";
+};
